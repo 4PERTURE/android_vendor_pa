22cb313f70c5ab1d86ea6ec6200b331d3af24dcb
4PERTURE <slicey2001@gmail.com>
2020-08-27 22:56:58 +0530

    add Mia Toll
    Change-Id: Icfc3d7f8f9a3d1ea344431ecfbb9efb45796858c

---
+++ products/miatoll/pa_miatoll.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_USES_GOOGLE_RECORDER := false
+
+PRODUCT_NAME := pa_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
